e0f592e3cb75fb183ab7bfdfaa6c2e8f677ce803
ds-sloth <72112344+ds-sloth@users.noreply.github.com>
2025-03-09 16:38:16 -0400

    archive_entry.h: remove archive_entry_copy_bhfi

--- libarchive/archive_entry.h
+++ libarchive/archive_entry.h
@@ -311,9 +311,6 @@ __LA_DECL int archive_entry_is_encrypted(struct archive_entry *);
 
 __LA_DECL void	archive_entry_set_atime(struct archive_entry *, time_t, long);
 __LA_DECL void  archive_entry_unset_atime(struct archive_entry *);
-#if defined(_WIN32) && !defined(__CYGWIN__)
-__LA_DECL void archive_entry_copy_bhfi(struct archive_entry *, BY_HANDLE_FILE_INFORMATION *);
-#endif
 __LA_DECL void	archive_entry_set_birthtime(struct archive_entry *, time_t, long);
 __LA_DECL void  archive_entry_unset_birthtime(struct archive_entry *);
 __LA_DECL void	archive_entry_set_ctime(struct archive_entry *, time_t, long);
